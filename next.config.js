065adee32f3c5a7f8c2cf0c2e9eb513d2cb5ed67
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2024-03-14 21:45:46 -0300

    config: I've added a setting that extends the File System routes of next

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Configuração para que o next aceite como páginas, arquivos terminados com os prefixos abaixo
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 }
 
 module.exports = nextConfig
